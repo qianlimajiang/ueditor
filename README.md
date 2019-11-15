c2c22649012b1165a37febe2aa2aa2993694edbd
刘江江 <469028589@qq.com>
2019-11-15 13:10:26 +0800

    '初步完成兼容云上传的Ueditor封装'

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# ueditor
+# 说明 
+此扩展时将Ueditor富文本编辑器的PHP上传代码重新封装，可支持阿里云/七牛云上传，默认配置文件在当前扩展下的src目录中，名为Config.php。
+配置可以自定义，例如tp5框架，将本配置文件内的$config数组变量复制到application/目录，记得要先新建配置文件，不要直接覆盖。
+默认配置未开启云上传，需将配置文件中的oss设置为true。
+如果使用了云端上传功能，需要下载云上传扩展包，比如阿里云oss的aliyuncs/oss-sdk-php。
+
+# 实例代码（tp5为例）
+namespace app\admin\controller;
+
+use OSS\OssClient;
+use OSS\Core\OssException;
+use UeditorExtend\Ueditor as UeditorExtendUeditor;
+
+class Ueditor extends Base
+{
+    public function index()
+    {
+        $get = $this->request->get();
+
+        $config = include(APP_PATH . '/admin/extra/ueditor.php');
+        //如果开启了oss则在这里增加判断即可，如果没有开启，可忽略此处代码
+        if ($config['oss'] && $_FILES && isset($_FILES['upfile']['tmp_name'])) {
+            $accessKeyId = "**************";
+            $accessKeySecret = "**************";
+            $endpoint = "oss-cn-beijing.aliyuncs.com";
+
+            try {
+                $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint);
+            } catch (OssException $e) {
+                print $e->getMessage();
+            }
+
+            $bucket = "******";
+            $object = uniqid() . '.jpg'; //文件名称
+            try {
+                $result = $ossClient->uploadFile($bucket, $object, $_FILES['upfile']['tmp_name']);
+            } catch (OssException $e) {
+                print $e->getMessage();
+            }
+            $res_url = $result['info']['url'];
+            
+            $get['action'] = 'catchimage';
+            $fieldName = $config['catcherFieldName'];
+            $_POST[$fieldName] = [$res_url];
+        }
+        //如果没有开启oss上传，直接写这两行代码即可
+        $ueditor = new UeditorExtendUeditor($get, $config);
+        $res = $ueditor->upload();
+
+        return json($res);
+    }
+}
---
+++ src/Ueditor.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace UeditorExtend;
+
+class Ueditor
+{
+    private $config;
+    private $action;
+    private $file;
+    private $http_get;
+    public function __construct($get, $config = [])
+    {
+        if ($config) {
+            $this->config = $config;
+        } else {
+            $this->config = Config::$config;
+        }
+        $this->action = $get['action'];
+        $this->http_get = $get;
+    }
+    public function upload()
+    {
+        $CONFIG = $this->config;
+        switch ($this->action) {
+            case 'config':
+                return $CONFIG;
+                break;
+                /* 上传图片 */
+            case 'uploadimage':
+                /* 上传涂鸦 */
+            case 'uploadscrawl':
+                /* 上传视频 */
+            case 'uploadvideo':
+                /* 上传文件 */
+            case 'uploadfile':
+                $result = $this->uploadFile();
+                break;
+
+                /* 列出图片 */
+            case 'listimage':
+                $result = $this->list();
+                break;
+                /* 列出文件 */
+            case 'listfile':
+                $result = $this->list();
+                break;
+
+                /* 抓取远程文件 */
+            case 'catchimage':
+                $result = $this->crawler();
+                break;
+
+            default:
+                $result = json_encode(array(
+                    'state' => '请求地址出错'
+                ));
+                break;
+        }
+        /* 输出结果 */
+        if (isset($_GET["callback"])) {
+            if (preg_match("/^[\w_]+$/", $_GET["callback"])) {
+                return htmlspecialchars($_GET["callback"]) . '(' . $result . ')';
+            } else {
+                return ['state' => 'callback参数不合法'];
+            }
+        } else {
+            return $result;
+        }
+    }
+    public function uploadFile()
+    {
+        $CONFIG = $this->config;
+        $base64 = "upload";
+        switch ($this->action) {
+            case 'uploadimage':
+                $config = array(
+                    "pathFormat" => $CONFIG['imagePathFormat'],
+                    "maxSize" => $CONFIG['imageMaxSize'],
+                    "allowFiles" => $CONFIG['imageAllowFiles']
+                );
+                $fieldName = $CONFIG['imageFieldName'];
+                break;
+            case 'uploadscrawl':
+                $config = array(
+                    "pathFormat" => $CONFIG['scrawlPathFormat'],
+                    "maxSize" => $CONFIG['scrawlMaxSize'],
+                    "allowFiles" => $CONFIG['scrawlAllowFiles'],
+                    "oriName" => "scrawl.png"
+                );
+                $fieldName = $CONFIG['scrawlFieldName'];
+                $base64 = "base64";
+                break;
+            case 'uploadvideo':
+                $config = array(
+                    "pathFormat" => $CONFIG['videoPathFormat'],
+                    "maxSize" => $CONFIG['videoMaxSize'],
+                    "allowFiles" => $CONFIG['videoAllowFiles']
+                );
+                $fieldName = $CONFIG['videoFieldName'];
+                break;
+            case 'uploadfile':
+            default:
+                $config = array(
+                    "pathFormat" => $CONFIG['filePathFormat'],
+                    "maxSize" => $CONFIG['fileMaxSize'],
+                    "allowFiles" => $CONFIG['fileAllowFiles']
+                );
+                $fieldName = $CONFIG['fileFieldName'];
+                break;
+        }
+        /* 生成上传实例对象并完成上传 */
+        $up = new UeditorUploader($fieldName, $config, $base64);
+
+        return $up->getFileInfo();
+    }
+    public function list()
+    {
+        $_GET = $this->http_get;
+        $CONFIG = $this->config;
+        switch ($this->action) {
+                /* 列出文件 */
+            case 'listfile':
+                $allowFiles = $CONFIG['fileManagerAllowFiles'];
+                $listSize = $CONFIG['fileManagerListSize'];
+                $path = $CONFIG['fileManagerListPath'];
+                break;
+                /* 列出图片 */
+            case 'listimage':
+            default:
+                $allowFiles = $CONFIG['imageManagerAllowFiles'];
+                $listSize = $CONFIG['imageManagerListSize'];
+                $path = $CONFIG['imageManagerListPath'];
+        }
+        $allowFiles = substr(str_replace(".", "|", join("", $allowFiles)), 1);
+        /* 获取参数 */
+        $size = isset($_GET['size']) ? htmlspecialchars($_GET['size']) : $listSize;
+        $start = isset($_GET['start']) ? htmlspecialchars($_GET['start']) : 0;
+        $end = $start + $size;
+
+        $path = $_SERVER['DOCUMENT_ROOT'] . (substr($path, 0, 1) == "/" ? "" : "/") . $path;
+        $files = $this->getfiles($path, $allowFiles);
+        if (!count($files)) {
+            return array(
+                "state" => "no match file",
+                "list" => array(),
+                "start" => $start,
+                "total" => count($files)
+            );
+        }
+        /* 获取指定范围的列表 */
+        $len = count($files);
+        for ($i = min($end, $len) - 1, $list = array(); $i < $len && $i >= 0 && $i >= $start; $i--) {
+            $list[] = $files[$i];
+        }
+        //倒序
+        //for ($i = $end, $list = array(); $i < $len && $i < $end; $i++){
+        //    $list[] = $files[$i];
+        //}
+
+        /* 返回数据 */
+        $result = array(
+            "state" => "SUCCESS",
+            "list" => $list,
+            "start" => $start,
+            "total" => count($files)
+        );
+
+        return $result;
+    }
+    public function crawler()
+    {
+        $CONFIG = $this->config;
+        set_time_limit(0);
+        /* 上传配置 */
+        $config = array(
+            "pathFormat" => $CONFIG['catcherPathFormat'],
+            "maxSize" => $CONFIG['catcherMaxSize'],
+            "allowFiles" => $CONFIG['catcherAllowFiles'],
+            "oriName" => "remote.png"
+        );
+        $fieldName = $CONFIG['catcherFieldName'];
+        /* 抓取远程图片 */
+        $list = array();
+        if (isset($_POST[$fieldName])) {
+            $source = $_POST[$fieldName];
+        } else {
+            $source = $_GET[$fieldName];
+        }
+        foreach ($source as $imgUrl) {
+            $item = new UeditorUploader($imgUrl, $config, "remote");
+            $info = $item->getFileInfo();
+            array_push($list, array(
+                "state" => $info["state"],
+                "url" => $info["url"],
+                "size" => $info["size"],
+                "title" => htmlspecialchars($info["title"]),
+                "original" => htmlspecialchars($info["original"]),
+                "source" => htmlspecialchars($imgUrl)
+            ));
+        }
+        if ($CONFIG['oss'] && count($list) > 0) { 
+            return $list[0];
+        } else {
+            /* 返回抓取数据 */
+            return array(
+                'state' => count($list) ? 'SUCCESS' : 'ERROR',
+                'list' => $list
+            );
+        }
+    }
+    /**
+     * 遍历获取目录下的指定类型的文件
+     * @param $path
+     * @param array $files
+     * @return array
+     */
+    public function getfiles($path, $allowFiles, &$files = array())
+    {
+        if (!is_dir($path)) return null;
+        if (substr($path, strlen($path) - 1) != '/') $path .= '/';
+        $handle = opendir($path);
+        while (false !== ($file = readdir($handle))) {
+            if ($file != '.' && $file != '..') {
+                $path2 = $path . $file;
+                if (is_dir($path2)) {
+                    $this->getfiles($path2, $allowFiles, $files);
+                } else {
+                    if (preg_match("/\.(" . $allowFiles . ")$/i", $file)) {
+                        $files[] = array(
+                            'url' => substr($path2, strlen($_SERVER['DOCUMENT_ROOT'])),
+                            'mtime' => filemtime($path2)
+                        );
+                    }
+                }
+            }
+        }
+        return $files;
+    }
+}
